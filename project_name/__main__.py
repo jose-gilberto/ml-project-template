cabcf9a550fa6eff2dbc8576835b4b73d6099207
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2023-05-19 15:00:49 -0300

    feat: project base folder

---
+++ project_name/__main__.py
@@ -0,0 +1,7 @@
+""" Entry point for project_name.
+"""
+
+from .cli import main # pragma: no cover
+
+if __name__ == '__main__': # pragma: no cover
+    main()
---
+++ project_name/cli.py
@@ -0,0 +1,26 @@
+""" CLI interface for project_name.
+
+- Install click or typer and create a CLI app
+- Use builtin argparse
+- Start training and testing scripts
+- Import things from .base module
+"""
+
+
+def main() -> None: # pragma: no cover
+    """
+    The main function executes on commands:
+    `python -m project_name` and `$ project_name`
+    
+    This is your program's entry point.
+    
+    You can change this function to do whatever you want.
+    
+    Examples:
+        - Run a test suite
+        - Run experiments
+        - Run Preprocessing data
+        - Run a command line application (Click, Typer, ArgParse)
+        - List all available tasks
+    """
+    print('This will do something.')
